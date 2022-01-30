8df0e08b850f7c99d3603128642cb64523e7473d
DV Studio <72212575+DvScriptsSK@users.noreply.github.com>
2022-01-30 18:03:24 +0100

    Add files via upload

--- server.lua
+++ server.lua
@@ -1,34 +1,35 @@
 local rentedVehicles = {}
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local QBCore = exports['qb-core']:GetCoreObject()
+local Player = QBCore.Functions.GetPlayer(source)
 
 RegisterNetEvent('sqz_carrental:RentVehicle')
 AddEventHandler('sqz_carrental:RentVehicle', function(model, insurance, price, time, rentalIndex)
     time = time:gsub('min', '')
     time =  tonumber(time)
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local xPlayer = QBCore.Functions.GetPlayer(source)
 
-    if xPlayer.getMoney() >= price + Config.DownPayment then
-        xPlayer.removeMoney(price + Config.DownPayment)
-        xPlayer.showNotification('You have paid '..price..'$')
-        xPlayer.showNotification('You have paid '..Config.DownPayment..'$ as a down payment')
+    if xPlayer.PlayerData.money.cash>= price + Config.DownPayment then
+        xPlayer.Functions.RemoveMoney('cash',price + Config.DownPayment)
+        TriggerClientEvent('QBCore:Notify', source,'You have paid '..price..'$')
+        TriggerClientEvent('QBCore:Notify', source,'You have paid '..Config.DownPayment..'$ as a down payment')
         TriggerClientEvent('sqz_carrental:SpawnVehicle', source, model, insurance, price, time, rentalIndex)
-    elseif xPlayer.getAccount('bank').money >= price + Config.DownPayment then
-        xPlayer.removeAccountMoney('bank', price + Config.DownPayment)
-        xPlayer.showNotification('You have paid '..Config.DownPayment..'$ as a down payment from your bank account')
-        xPlayer.showNotification('You have paid '..price..'$ from your bank account')
+    elseif Player.PlayerData.money['bank'] .money >= price + Config.DownPayment then
+        xPlayer.Functions.RemoveMoney('bank', price + Config.DownPayment)
+        TriggerClientEvent('QBCore:Notify', source,'You have paid '..Config.DownPayment..'$ as a down payment from your bank account')
+        TriggerClientEvent('QBCore:Notify', source,'You have paid '..price..'$ from your bank account')
         TriggerClientEvent('sqz_carrental:SpawnVehicle', source, model, insurance, price, time, rentalIndex)
     else
-        xPlayer.showNotification('You can not afford renting this vehicle')
+        TriggerClientEvent('QBCore:Notify', source,'You can not afford renting this vehicle')
     end
 end)
 
 RegisterNetEvent('sqz_carrental:VehicleSpawned')
 AddEventHandler('sqz_carrental:VehicleSpawned', function(plate, insurance, time, netId)
     local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
+    local xPlayer = QBCore.Functions.GetPlayer(_source)
     if not rentedVehicles[plate] then
         rentedVehicles[plate] = {
-            owner = xPlayer.identifier,
+            owner = xPlayer.PlayerData.license,
             insurance = insurance,
             netId = netId,
             downPayment = Config.DownPayment
@@ -44,9 +45,9 @@ AddEventHandler('sqz_carrental:VehicleSpawned', function(plate, insurance, time,
                             if rentedVehicles[plate].downPayment >= Config.ExtraChargePerMinute then
                                 rentedVehicles[plate].downPayment = rentedVehicles[plate].downPayment - Config.ExtraChargePerMinute
                             else
-                                if ESX.GetPlayerFromId(_source) then
-                                    xPlayer.showNotification('The deposit will not be refunded, because you have not returned the vehicle')
-                                    xPlayer.showNotification('The vehicle has been impounded')
+                                if QBCore.Functions.GetPlayer(_source) then
+                                    TriggerClientEvent('QBCore:Notify', source,'The deposit will not be refunded, because you have not returned the vehicle')
+                                    TriggerClientEvent('QBCore:Notify', source,'The vehicle has been impounded')
                                 end
                                 DeleteEntity(NetworkGetEntityFromNetworkId(netId))
                                 rentedVehicles[plate] = nil
@@ -64,14 +65,14 @@ end)
 
 RegisterNetEvent('sqz_carrental:ReturnVehicle')
 AddEventHandler('sqz_carrental:ReturnVehicle', function(plate, damageIndex)
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local xPlayer = QBCore.Functions.GetPlayer(source)
     if not rentedVehicles[plate] then
-        xPlayer.showNotification('You can not return this vehicle because this one has not been rented.')
+        TriggerClientEvent('QBCore:Notify', source,'You can not return this vehicle because this one has not been rented.')
         return
     end
 
-    if rentedVehicles[plate].owner ~= xPlayer.identifier then
-        xPlayer.showNotification('You can not return this vehicle because you are not borrower.')
+    if rentedVehicles[plate].owner ~= xPlayer.PlayerData.license then
+        TriggerClientEvent('QBCore:Notify', source,'You can not return this vehicle because you are not borrower.')
         return
     end
 
@@ -83,14 +84,14 @@ AddEventHandler('sqz_carrental:ReturnVehicle', function(plate, damageIndex)
 
     if damageIndex < 1 then
         local reducedBy = Config.DownPayment - Config.DownPayment * damageIndex
-        xPlayer.showNotification('Down payment you should receive has been lowered by '..reducedBy..'$ because you have returned the vehicle damaged')
+        TriggerClientEvent('QBCore:Notify', source,'Down payment you should receive has been lowered by '..reducedBy..'$ because you have returned the vehicle damaged')
     end
 
-    xPlayer.addAccountMoney('bank', moneyToGive)
-    xPlayer.showNotification('The down payment of amount '..moneyToGive..'$ has been returned you.')
+    xPlayer.Functions.AddMoney('bank', moneyToGive)
+    TriggerClientEvent('QBCore:Notify', source,'The down payment of amount '..moneyToGive..'$ has been returned you.')
     TaskLeaveVehicle(GetPlayerPed(source), NetworkGetEntityFromNetworkId(rentedVehicles[plate].netId), 0)
     Wait(1700)
     DeleteEntity(NetworkGetEntityFromNetworkId(rentedVehicles[plate].netId))
     rentedVehicles[plate] = nil
-    TriggerClientEvent('sqz_carrental:VehicleSuccessfulyReturned', xPlayer.source)
+    TriggerClientEvent('sqz_carrental:VehicleSuccessfulyReturned', xPlayer.PlayerData.source)
 end)
